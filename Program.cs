163d10369710409e9211201906dd14e8f32a3530
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-12-04 21:14:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var moreInput = true;
+            var puzzleInput = new List<int>();
+            while (moreInput)
+            {
+                var x = Console.ReadLine();
+                if (x == "")
+                {
+                    moreInput = false;
+                }
+                else 
+                {
+                    puzzleInput.Add(int.Parse(x));
+                }
+
+            }
+            for (int i = 0; i < puzzleInput.Count-2; i++)
+            {
+                for (int j = i+1; j < puzzleInput.Count-1; j++)
+                {
+                    for (int k = j+1; k < puzzleInput.Count; k++)
+                    {
+                    if (puzzleInput[i]+puzzleInput[j] + puzzleInput[k] == 2020)
+                    {
+                        Console.WriteLine("answer: " + puzzleInput[i] * puzzleInput[j]* puzzleInput[k]);
+                    }
+                    }
+                }
+            }
+        }
+    }
+}
